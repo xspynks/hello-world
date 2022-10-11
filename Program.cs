407fd7a10f9c312ab1f34cc73e7fd67cced3e7dc
Rafael de Paula <xspynks@gmail.com>
2022-10-11 14:49:49 -0300

    feat: remove variaveis desnecessárias, cria input para número e altera output

--- Program.cs
+++ Program.cs
@@ -2,6 +2,11 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!\n" + DateTime.Now.ToString("MMMM, dd, yyyy"));
 
-int i = 30;
-float f = 3.14f;
-double d = 14.3d;
+Console.WriteLine("Digite um número:");
+int age = Convert.ToInt32(Console.ReadLine());
+
+// receives input from the user
+Console.WriteLine("Digite seu nome: ");
+string name = Console.ReadLine();
+
+Console.WriteLine("Olá, " + name + ". Sua idade é: " + age);
