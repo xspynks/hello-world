34ab29378cf067d379777a4b21db454bc5c9c821
Rafael de Paula <xspynks@gmail.com>
2022-10-11 14:38:22 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!\n" + DateTime.Now.ToString("MMMM, dd, yyyy"));
+
+int i = 30;
+float f = 3.14f;
+double d = 14.3d;
